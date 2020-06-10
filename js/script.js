0b99f010de14b8f9ff0b57a03c9a6f7b7561e359
Tommaso Venza <tommasovenza@gmail.com>
2020-06-10 21:02:57 +0200

    update functions

--- js/script.js
+++ js/script.js
@@ -1,11 +1,26 @@
 $(document).ready(
     function () {
 
+        // all'evento mouseEnter sull'li con classe .with-dropdown i figli dello stesso con classe dropdown aggiungi classe open
         $('.with-dropdown').mouseenter(
             function () {
                 $(this).children('.dropdown').toggleClass('open');
             }
         ); 
+
+        // all'evento click sull'li con classe .with-dropdown i figli dello stesso con classe dropdown aggiungi classe open
+        $('.with-dropdown').click(
+            function () {
+                $(this).children('.dropdown').removeClass('open');
+            }
+        );
+        
+        // all'evento mouseLeave sull'li con classe .with-dropdown i figli dello stesso con classe dropdown aggiungi classe open
+        $('.with-dropdown').mouseleave(
+            function () {
+                $(this).children('.dropdown').removeClass('open');
+            }
+        );
 
     }
 );
